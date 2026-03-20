cb8f3ce3c43e2622d515063921d04328e8d2351a
Claes Fransson <81clafra@gafe.molndal.se>
2026-03-20 14:34:05 +0100

    Add remaining shared view files and static files for the web app, by Qwen3.5-27b.

--- src/TodoApp/wwwroot/js/site.js
+++ src/TodoApp/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Todo App JavaScript
 
-// Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function () {
+    // Add any custom JavaScript functionality here
+    console.log('Todo App loaded');
+});
